d293e93a9f9ce5e7f34bb55023fcaaf82c250f56
Marcos Aparicio <marcos.aparicio593@gmail.com>
2021-08-28 17:23:53 -0400

    First commit, still not organized

---
+++ script.js
@@ -0,0 +1,122 @@
+//variables to get canvas
+var paper = document.getElementById('canvas');
+var canvas = paper.getContext('2d');
+var pepito = paper.addEventListener('click', game);
+//variables for tic tac toe's squares
+var circle = {
+    url: './o.png',
+    spaces: new Array()
+}
+var equis = {
+    url: './x.png',
+    spaces: new Array()
+}
+//extra variables
+var p = document.getElementById('win');
+var victory = false;
+circle.image = new Image();
+circle.image.src = circle.url;
+equis.image = new Image();
+equis.image.src = equis.url;
+var y;
+var x;
+var turno = 0; // Initial Turn
+var free = Array(9).fill(true) //Array for each square's availability 
+
+//drawing the Tic Tac Toe Lines
+draw(canvas, 'black', 1, 201, 0, 201, 602);
+draw(canvas, 'black', 1, 402, 0, 402, 602);
+draw(canvas, 'black', 1, 0, 201, 602, 201);
+draw(canvas, 'black', 1, 0, 402, 602, 402);
+
+
+function game(a) {
+    //getting coordinates from canvas
+    x = a.offsetX;
+    y = a.offsetY;
+    //each condition represents the coordinates for each square
+    if (x >= 0 & x < 201 & y >= 0 & y < 201) {
+        drawMichi(0, 0, 0);
+    }
+    if (x > 201 & x < 402 & y >= 0 & y < 201) {
+        drawMichi(201, 0, 1);
+    }
+    if (x > 402 & x <= 602 & y >= 0 & y < 201) {
+        drawMichi(402, 0, 2);
+    }
+    if (x >= 0 & x < 201 & y > 201 & y < 402) {
+        drawMichi(0, 201, 3);
+    }
+    if (x > 201 & x < 402 & y > 201 & y < 402) {
+        drawMichi(201, 201, 4);
+    }
+    if (x > 402 & y <= 602 & y > 201 & y < 402) {
+        drawMichi(402, 201, 5);
+    }
+    if (x >= 0 & x < 201 & y > 402 & y <= 602) {
+        drawMichi(0, 402, 6);
+    }
+    if (x > 201 & x < 402 & y > 402 & y <= 602) {
+        drawMichi(201, 402, 7);
+    }
+    if (x > 402 & y <= 602 & y > 402 & y <= 602) {
+        drawMichi(402, 402, 8);
+    }
+}
+
+//this function is in charge of drawing the simbol and verifying its just one on each square
+function drawMichi(x, y, square) {
+    if (turno & free[square]) {
+        canvas.drawImage(equis.image, x, y)
+        equis.spaces[square] = true;
+        turno = 0;
+        console.log(equis.spaces);
+        winningConditions(equis,'X')
+    }
+    else if (!turno & free[square]) {
+        canvas.drawImage(circle.image, x, y)
+        circle.spaces[square] = true;
+        turno = 1;
+        console.log(circle.spaces);
+        winningConditions(circle,'Circle');
+    }
+    else {
+        if (victory) {
+            alert('Someone has already won')
+        }
+        else {
+            alert('Choose another slot');
+        }
+    }
+    free[square] = false;
+}
+//this function works with the previous ones, detects if someone won in order to finish the game
+function winningConditions(player,name) {
+    if (player.spaces[0] & player.spaces[1] & player.spaces[2] ||
+        player.spaces[0] & player.spaces[3] & player.spaces[6] ||
+        player.spaces[0] & player.spaces[4] & player.spaces[8] ||
+        player.spaces[1] & player.spaces[4] & player.spaces[7] ||
+        player.spaces[2] & player.spaces[4] & player.spaces[6] ||
+        player.spaces[2] & player.spaces[5] & player.spaces[8] ||
+        player.spaces[3] & player.spaces[4] & player.spaces[5] ||
+        player.spaces[6] & player.spaces[7] & player.spaces [8]) {
+        win(name);
+    }
+}
+function win(winner) {
+    p.innerHTML = winner + ' won the game';
+    for (e in free) {
+        free[e] = false;
+    }
+    victory = true;
+}
+function draw(context, color, width, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = color;
+    context.lineWidth = width;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath;
+
+}
